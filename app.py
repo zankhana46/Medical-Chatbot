be1dbf310cfda9ad4b084e6ac3635b0d193386b3
zankhana mehta <86908588+zankhana46@users.noreply.github.com>
2025-01-31 17:48:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ import google.generativeai as gen_ai
 import google.ai.generativelanguage as glm
 from PIL import Image
 from streamlit_option_menu import option_menu
-from beyondllm import source, retrieve, embeddings, llms, generator
-from beyondllm.llms import GeminiModel
-from beyondllm.embeddings import GeminiEmbeddings
 import warnings 
 warnings.filterwarnings('ignore')
 
@@ -69,7 +66,7 @@ if "chat_session" not in st.session_state:
 with st.sidebar:
     selected = option_menu(
         menu_title="Swasthya",
-        options=["DocBot", "Chat with Report"],
+        options=["DocBot", "VisionBot"],
         icons=["robot", "eye"],
         default_index=0,
         orientation="vertical",
@@ -88,8 +85,8 @@ with st.sidebar:
 
     if selected == "DocBot":
         st.write(" **DocBot** - Engage in text-based medical conversations.")
-    elif selected == "Chat with Report":
-        st.write("📄 **Chat with Report** - Ask questions about uploaded documents.")
+    elif selected == "VisionBot":
+        st.write("👁 **VisionBot** - Analyze and interpret with images.")
 
 if selected == "DocBot":
     # Display the chatbot's title on the page
@@ -119,38 +116,81 @@ if selected == "DocBot":
         with st.chat_message("assistant"):
             st.markdown(gemini_response.text)
 
-elif selected == "Chat with Report":
-    st.title("📄 Chat with Report")
-    # Ensure the API key is loaded correctly
-    google_api_key = os.getenv("GOOGLE_API_KEY")
-    if not google_api_key:
-        st.error("Please set the GOOGLE_API_KEY environment variable.")
-        st.stop()
+# elif selected == "Chat with Report":
+#     st.title("📄 Chat with Report")
+#     # Ensure the API key is loaded correctly
+#     google_api_key = os.getenv("GOOGLE_API_KEY")
+#     if not google_api_key:
+#         st.error("Please set the GOOGLE_API_KEY environment variable.")
+#         st.stop()
 
-    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-    question = st.text_input("Enter your question")
+#     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+#     question = st.text_input("Enter your question")
 
-    system_prompt = """
-    You are a medical bot and a professional doctor from Harvard Medical who answers user queries from the given CONTEXT.
-    You help users understand and analyze medical reports. You can extract key findings, explain medical terminology, and answer questions related to medicine, surgery, diseases, illness etc. 
-    You are honest, to the point, coherent and don't hallucinate.
-    If the user query is not in context, simply tell `We are sorry, we don't have information on this`.
-    """
+#     system_prompt = """
+#     You are a medical bot and a professional doctor from Harvard Medical who answers user queries from the given CONTEXT.
+#     You help users understand and analyze medical reports. You can extract key findings, explain medical terminology, and answer questions related to medicine, surgery, diseases, illness etc. 
+#     You are honest, to the point, coherent and don't hallucinate.
+#     If the user query is not in context, simply tell `We are sorry, we don't have information on this`.
+#     """
 
-    if uploaded_file is not None and question:
-        save_path = "./uploaded_files"
-        os.makedirs(save_path, exist_ok=True)  # Create directory if it doesn't exist
-        file_path = os.path.join(save_path, uploaded_file.name)
+#     if uploaded_file is not None and question:
+#         save_path = "./uploaded_files"
+#         os.makedirs(save_path, exist_ok=True)  # Create directory if it doesn't exist
+#         file_path = os.path.join(save_path, uploaded_file.name)
 
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
+#         with open(file_path, "wb") as f:
+#             f.write(uploaded_file.getbuffer())
 
-        data = source.fit("/Users/zankhanamehta/Documents/Projects/Swasthya-DocBot/uploaded_files/Shubham Tejani_Male_21_2024_6 (2).pdf", dtype="pdf", chunk_size=1024, chunk_overlap=250)
-        embed_model = GeminiEmbeddings(api_key=google_api_key, model_name="models/embedding-001")
-        llm = GeminiModel(model_name="gemini-pro", google_api_key=google_api_key)
-        retriever = retrieve.auto_retriever(data, type="normal", top_k=3, embed_model=embed_model)
-        pipeline = generator.Generate(question=question, system_prompt=system_prompt, retriever=retriever, llm=llm)
-        response = pipeline.call()
-        st.write(response)
-    
+#         data = source.fit("/Users/zankhanamehta/Documents/Projects/Swasthya-DocBot/uploaded_files/Shubham Tejani_Male_21_2024_6 (2).pdf", dtype="pdf", chunk_size=1024, chunk_overlap=250)
+#         embed_model = GeminiEmbeddings(api_key=google_api_key, model_name="models/embedding-001")
+#         llm = GeminiModel(model_name="gemini-pro", google_api_key=google_api_key)
+#         retriever = retrieve.auto_retriever(data, type="normal", top_k=3, embed_model=embed_model)
+#         pipeline = generator.Generate(question=question, system_prompt=system_prompt, retriever=retriever, llm=llm)
+#         response = pipeline.call()
+#         st.write(response)
 
+elif selected == "VisionBot":
+    st.header("👁 Visionbot™")
+    st.write("")
+
+    image_prompt = st.text_input("Interact with the Image", placeholder="Prompt", label_visibility="visible")
+    uploaded_file = st.file_uploader("Choose an Image", accept_multiple_files=False, type=["png", "jpg", "jpeg", "img", "webp"])
+
+    if uploaded_file is not None:
+        st.image(Image.open(uploaded_file), use_column_width=True)
+        st.markdown("""
+            <style>
+            img {
+                border-radius: 10px;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+
+    if st.button("GET RESPONSE", use_container_width=True):
+        # Update model name to gemini-1.5-flash
+        model = gen_ai.GenerativeModel("gemini-1.5-flash")
+        
+        if uploaded_file is not None:
+            if image_prompt != "":
+                image = Image.open(uploaded_file)
+                
+                # Simplified content creation
+                response = model.generate_content(
+                    [
+                        image_prompt,
+                        image
+                    ],
+                    safety_settings=safety_settings
+                )
+                
+                st.write("")
+                st.write(":blue[Response]")
+                st.write("")
+                st.markdown(response.text)
+            else:
+                st.write("")
+                st.header(":red[Please Provide a prompt]")
+        else:
+            st.write("")
+            st.header(":red[Please Provide an image]")
